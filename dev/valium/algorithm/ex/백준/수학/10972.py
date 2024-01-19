77de2af3af17eef6f55cf288b1d269e32cf0392c
Valium-K <47807782+Valium-K@users.noreply.github.com>
2024-01-19 19:45:17 +0900

    add : 백준 10972

---
+++ dev/valium/algorithm/ex/백준/수학/10972.py
@@ -0,0 +1,25 @@
+'''
+https://www.acmicpc.net/problem/10972
+'''
+
+def solve(n, arr):
+    for i in range(n-1, 0, -1):
+        if arr[i-1] < arr[i]:
+            target_index = arr.index(sorted(list(filter(lambda x: x >= 0, map(lambda x: x - arr[i-1], arr[i:]))))[0] + arr[i-1])
+
+            temp = arr[i-1]
+            arr[i-1] = arr[target_index]
+            arr[target_index] = temp
+
+            for j in arr[:i]:
+                print(j, end=' ')
+            for j in sorted(arr[i:]):
+                print(j, end=' ')
+
+            return
+    print(-1)
+if __name__ == '__main__':
+    n = int(input())
+    arr = list(map(int, input().split()))
+
+    solve(n, arr)
