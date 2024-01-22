4069c604d4275bac8d04be17e2b6b4f9d6576c06
Valium-K <47807782+Valium-K@users.noreply.github.com>
2024-01-22 21:07:28 +0900

    update : 백준 17626

--- dev/valium/algorithm/ex/백준/다이나믹 프로그래밍/17626.py
+++ dev/valium/algorithm/ex/백준/다이나믹 프로그래밍/17626.py
@@ -1,26 +1,45 @@
 '''
 https://www.acmicpc.net/problem/17626
-- 시간초과 -
 '''
 
-import math, itertools
+# import math, itertools
+
+# if __name__ == '__main__':
+#   n = int(input())
+
+#   range = [(i + 1) * (i + 1) for i in range(math.floor(math.sqrt(n)))]
+#   reversed_range = sorted(range, reverse = True)
+  
+#   is_fin = False
+#   for i, _ in enumerate(reversed_range):
+#     cur_combinations = itertools.combinations(reversed_range, i + 1)
+    
+#     for combination in cur_combinations:
+
+#       if(sum(combination) == n):
+#         print(i + 1)
+#         is_fin = True
+#         break
+
+#     if(is_fin):
+#       break
+
+# #################################################################
+
+import sys
 
 if __name__ == '__main__':
-  n = int(input())
-
-  range = [(i + 1) * (i + 1) for i in range(math.floor(math.sqrt(n)))]
-  reversed_range = sorted(range, reverse = True)
+  n = int(sys.stdin.readline())
   
-  is_fin = False
-  for i, _ in enumerate(reversed_range):
-    cur_combinations = itertools.combinations(reversed_range, i + 1)
+  cache = [0 for _ in range(max(2, n + 1))]
+  cache[1] = 1
+  
+  for i in range(2, n + 1):
+      target = 4
+      for j in range(1, n):
+          if j ** 2 > i:
+              break
+          target = min(target, cache[i - (j**2)])
+      cache[i] = target + 1
     
-    for combination in cur_combinations:
-
-      if(sum(combination) == n):
-        print(i + 1)
-        is_fin = True
-        break
-
-    if(is_fin):
-      break
+  print(cache[n])
